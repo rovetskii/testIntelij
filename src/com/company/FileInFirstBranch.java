02e4d517474817c79ca65629597319600bb5e655
Ivan Rovetskyi <ivan.rovetskyi@intertechgroup.biz>
2021-06-07 11:24:44 +0300

    Added constructor method

--- src/com/company/FileInFirstBranch.java
+++ src/com/company/FileInFirstBranch.java
@@ -1,4 +1,10 @@
 package com.company;
 
 public class FileInFirstBranch {
+    public FileInFirstBranch() {
+        System.out.println("added constructor");
+    }
+    public void getMessage() {
+        System.out.println("added method");
+    }
 }
